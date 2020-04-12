6c873f22447f569ce6095bba1bf315f3bddee0f5
Felix <felixatika@gmail.com>
2020-04-12 14:49:40 -0400

    add .show and .hide methods to what we do section

--- js/script.js
+++ js/script.js
@@ -1,15 +1,7 @@
 $(document).ready(function () {
-  $("#design-image").click(function () {
-    $("#design-image").slideDown("1500").hide("1000");
-  });
-  $("#design").click(function () {
-    $("#design").slideUp("1500");
-    // $("#design-image").slideDown("1500");
-  });
-});
-$(document).ready(function () {
   $("#development-image").click(function () {
     $("#development-image").slideDown("1500").hide("1000");
+    $("#development").show("1500");
   });
   $("#development").click(function () {
     $("#development").slideUp("1500");
@@ -18,8 +10,21 @@ $(document).ready(function () {
 });
 
 $(document).ready(function () {
+  $("#design-image").click(function () {
+    $("#design-image").slideDown("1500").hide("1000");
+    $("#design").show("1500");
+  });
+  $("#design").click(function () {
+    $("#design").slideUp("1500");
+    $("#design-image").slideDown("1500");
+    // $("#design-image").slideDown("1500");
+  });
+});
+
+$(document).ready(function () {
   $("#product-image").click(function () {
     $("#product-image").slideDown("1500").hide("1000");
+    $("#product").show("1500");
   });
   $("#product").click(function () {
     $("#product").slideUp("1500");
