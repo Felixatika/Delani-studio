f1b27237eb80cd3f8e63d42310aa52bcc62f7c84
Felix <felixatika@gmail.com>
2020-04-12 15:39:53 -0400

    add overlays effect on all pictures in js

--- js/script.js
+++ js/script.js
@@ -38,3 +38,52 @@ $(document).ready(function () {
     $("#overlay").hide();
   });
 });
+$(document).ready(function () {
+  $("#pic2").mouseover(function () {
+    $("#overlay1").show();
+  }).mouseout(function () {
+    $("#overlay1").hide();
+  });
+});
+$(document).ready(function () {
+  $("#pic3").mouseover(function () {
+    $("#overlay2").show();
+  }).mouseout(function () {
+    $("#overlay2").hide();
+  });
+});
+$(document).ready(function () {
+  $("#pic4").mouseover(function () {
+    $("#overlay3").show();
+  }).mouseout(function () {
+    $("#overlay3").hide();
+  });
+});
+$(document).ready(function () {
+  $("#pic5").mouseover(function () {
+    $("#overlay4").show();
+  }).mouseout(function () {
+    $("#overlay4").hide();
+  });
+});
+$(document).ready(function () {
+  $("#pic6").mouseover(function () {
+    $("#overlay5").show();
+  }).mouseout(function () {
+    $("#overlay5").hide();
+  });
+});
+$(document).ready(function () {
+  $("#pic7").mouseover(function () {
+    $("#overlay6").show();
+  }).mouseout(function () {
+    $("#overlay6").hide();
+  });
+});
+$(document).ready(function () {
+  $("#pic8").mouseover(function () {
+    $("#overlay7").show();
+  }).mouseout(function () {
+    $("#overlay7").hide();
+  });
+});
